92efb079bae5c756f0d3066854f01d0949cb1a0d
Mukesh Joshi <mukesh.bciit@gmail.com>
2019-12-08 22:43:01 +0100

    Removed old projects

--- EmailSms/src/main/java/com/projects/email_sms/MailType.java
+++
@@ -1,7 +0,0 @@
-package com.projects.email_sms;
-
-public enum MailType {
-    GMAIL_SSL,
-    GMAIL_TLS,
-    LIVE
-}
--- SpringBoot/src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO 
-	TBL_EMPLOYEES (first_name, last_name, email) 
-VALUES
-  	('Kusum', 'Negi', 'kusum@gmail.com'),
-  	('Mukesh', 'Joshi', 'mukesh@gmail.com');
